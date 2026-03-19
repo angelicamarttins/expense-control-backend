40aa9da98a0beaabdbbf7ef451685953c1ce8059
Angélica Martins <marttinsangelica@gmail.com>
2026-03-19 08:34:38 -0300

    feat: create Category entity

---
+++ src/main/java/com/angelicamartins/expensecontrol/model/Category.java
@@ -0,0 +1,41 @@
+package com.angelicamartins.expensecontrol.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "category")
+public class Category {
+
+  @Id
+  @GeneratedValue
+  @Column(name = "category_id", nullable = false, updatable = false)
+  private UUID categoryId;
+
+  @Column(name = "category_name", nullable = false)
+  private String categoryName;
+
+  @Column(name = "default_category", nullable = false)
+  private Boolean defaultCategory;
+
+  @Column(name = "created_at", nullable = false, updatable = false)
+  private LocalDateTime created_at;
+
+  @Column(name = "updated_at")
+  private LocalDateTime updated_at;
+
+  @Column(name = "deleted_at", nullable = false, updatable = false)
+  private LocalDateTime deleted_at;
+
+}
